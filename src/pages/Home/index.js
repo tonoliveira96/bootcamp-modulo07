2d1c5cd3e3d3f6bc8113af6f7c7f029cea3e31b3
tonoliveira96 <ton.oliveira@sempre.facig.edu.br>
2020-04-03 14:10:43 -0300

    Estilização da Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import {MdShoppingBasket} from 'react-icons/md'
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-ez-flex-2-masculino/66/D16-4581-266/D16-4581-266_zoom2.jpg?ts=1570129001&ims=326x" alt="Tenis"/>
+        <strong>Tênis Cool</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingBasket sixe={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-ez-flex-2-masculino/66/D16-4581-266/D16-4581-266_zoom2.jpg?ts=1570129001&ims=326x" alt="Tenis"/>
+        <strong>Tênis Cool</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingBasket sixe={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-ez-flex-2-masculino/66/D16-4581-266/D16-4581-266_zoom2.jpg?ts=1570129001&ims=326x" alt="Tenis"/>
+        <strong>Tênis Cool</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingBasket sixe={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-ez-flex-2-masculino/66/D16-4581-266/D16-4581-266_zoom2.jpg?ts=1570129001&ims=326x" alt="Tenis"/>
+        <strong>Tênis Cool</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingBasket sixe={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carinho</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-ez-flex-2-masculino/66/D16-4581-266/D16-4581-266_zoom2.jpg?ts=1570129001&ims=326x" alt="Tenis"/>
+        <strong>Tênis Cool</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdShoppingBasket sixe={16} color="#fff" />
+          </div>
+          <span>Adicionar ao carinho</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
